2f0e0322da09f831c3d454f26090111473b9cc8e
Imrancu <mdimran3772@gmail.com>
2024-02-27 01:16:09 +0600

    completed

--- src/Routes/routes.jsx
+++ src/Routes/routes.jsx
@@ -90,7 +90,7 @@ import AdminHome from "../pages/Dashboard/AdminHome/AdminHome";
         {
           path: 'updateItem/:id',
           element: <AdminRoute><UpdateItem></UpdateItem></AdminRoute>,
-          loader: ({params}) => fetch(`http://localhost:5000/menu/${params.id}`)
+          loader: ({params}) => fetch(`https://bistro-boss-server-pink-kappa.vercel.app/menu/${params.id}`)
         },
         {
           path: 'users',
